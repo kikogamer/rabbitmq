805f9431999a1c36b06722b5a3c72f83a421360a
Ronaldo <kikogamer@gmail.com>
2024-06-14 12:43:38 -0300

    enviando mensagem para uma fila existente

---
+++ created-queue/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory() { HostName = "localhost", UserName = "logUser", Password = "logPwd", VirtualHost = "EnterpriseLog" };
+
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    var message = "Hello World!";
+    var body = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(exchange: "",
+                         routingKey: "hello",
+                         basicProperties: null,
+                         body: body);
+}
