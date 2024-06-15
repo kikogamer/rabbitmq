de0f647b5cdb59757d99b94975a3482579fc27dc
Ronaldo <kikogamer@gmail.com>
2024-06-14 23:24:45 -0300

    criando fila e enviando mensagem

---
+++ creating-queue/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory() { HostName = "localhost", UserName = "logUser", Password = "logPwd", VirtualHost = "EnterpriseLog" };
+
+using(var connection = factory.CreateConnection())
+using(var channel = connection.CreateModel())
+{
+    channel.QueueDeclare(queue: "nova-fila",
+                         durable: false,
+                         exclusive: false,
+                         autoDelete: false,
+                         arguments: null);
+    string message = "Hello World!";
+    var body = Encoding.UTF8.GetBytes(message);
+    channel.BasicPublish(exchange: "",
+                         routingKey: "nova-fila",
+                         basicProperties: null,
+                         body: body);
+}
